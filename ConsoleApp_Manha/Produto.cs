ac9ca95954410dd8b7b09bac5fca2193ec5570c8
M4NT <yan0507@live.com>
2023-03-08 08:50:46 -0300

    Create more class info

---
+++ ConsoleApp_Manha/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Manha
+{
+    public class Produto
+    {
+        private int id;
+        private string Descricao;
+        private decimal Valor;
+
+        public Produto(int id, string descricao, decimal valor)
+        {
+            this.id = id;
+            Descricao = descricao;
+            Valor = valor;
+        }
+    }
+}
