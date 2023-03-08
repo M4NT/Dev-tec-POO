6871fc7411862b8ef5513b9c1c4a40f32d29a581
M4NT <yan0507@live.com>
2023-03-08 10:16:31 -0300

    The end of the Products.

--- ConsoleApp_Manha/Produto.cs
+++ ConsoleApp_Manha/Produto.cs
@@ -4,19 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp_Manha
+namespace Aula_Desenvolvimento_de_Servidores
 {
     public class Produto
     {
-        private int id;
+        private int Id;
         private string Descricao;
         private decimal Valor;
 
         public Produto(int id, string descricao, decimal valor)
         {
-            this.id = id;
-            this.Descricao = descricao;
-            this.Valor = valor;
+            Id = id;
+            Descricao = descricao;
+            Valor = valor;
         }
+
+        public string getDescricao()
+        {
+            return Descricao;
+        }
+
+        public void setDescricao(string descricao)
+        {
+            if (descricao != "")
+            {
+                Descricao = descricao;
+            }
+        }
+        public decimal getValor()
+        {
+            return Valor;
+        }
+
+        public void setValor(decimal valor)
+        {
+            if (valor > 0)
+            {
+                Valor = valor;
+            }
+        }
+        public int getId()
+        {
+            return Id;
+        }
+
+        public void setId(int id)
+        {
+            if (id > 0)
+            {
+                Id = id;
+            }
+        }
+
     }
 }
--- ConsoleApp_Manha/Program.cs
+++ ConsoleApp_Manha/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Aula_Desenvolvimento_de_Servidores;
+
 Console.WriteLine("Hello, World!");
 
+Produto p1 = new Produto(1, "mouse sem fio", 59.90M);
+Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
